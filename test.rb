e8be6b80f0cbeedd0d11b32cf0fd8279c5ddc06f
Tobias Fritz <herr_fritz@hotmail.de>
2012-01-31 17:07:56 +0100

    commit of the words

--- test.rb
+++ test.rb
@@ -1,19 +1,56 @@
-target = 'Oro'
-ungeraten = target
-geraten = target.tr(ungeraten,'_')
-while geraten != target do
-	char = gets[0]
-	ungeraten = ungeraten.tr(char, '')
-	geraten = target.tr(ungeraten, '_')
-	puts geraten
+# The words come from file
+wordsfile = open("words.txt")
+a = wordsfile.read(wordsfile.size).to_s
+a = a.scan(/\w+/)
+target = ""
+while(target == "") do
+i = 10
+word = ""
+while (word.size < 10) do
+	word = a[(Time.now.to_i + i) % a.size]	
+	i = i+200
 end
-puts 'Awesome'
-#a.tr('zielwort - bereits eingegebene Buchstaben', '_')
-#while 
-#target zusammen != input
-#get zeichen
-#lookup alle vorkommen von zeichen position in target
-#write zeichen in output an stelle wo zeichen in target steht
-#puts 'Hello World!'
+if word[0].upcase! == nil
+	target = word
+end
+end
+
+# Now we have a word
+losecounter = 10
+puts "Set the difficulty h for hard, m for medium, e for easy"
+difficulty = gets[0]
+case difficulty
+when 'h'
+	losecounter = 5
+when 'm'
+	losecounter = 10
+when 'e'
+	losecounter = 15
+else
+end
+puts "GoGoGo"
+ungeraten = target
+geraten = target.tr(ungeraten,'_') 
+puts geraten
+while geraten != target && losecounter != 0 do
+	char = gets[0]
+	ungeraten = ungeraten.tr(char.downcase, '')
+	ungeraten = ungeraten.tr(char.upcase, '')
+	temp = geraten
+	geraten = target.tr(ungeraten, '_')
+	if temp != geraten
+		puts geraten
+	else	
+		puts geraten
+		losecounter = losecounter-1
+		puts "Try again!" + losecounter.to_s
+	end
+end
+if losecounter > 0
+	puts 'Awesome!'
+else
+	puts 'You lose!'
+	puts target + " would have been the solution."
+end
 
 
