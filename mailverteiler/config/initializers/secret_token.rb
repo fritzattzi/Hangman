e08ff529393bf977aea3a56706ff0df05b5d322a
Tobias Fritz <herr_fritz@hotmail.de>
2012-02-09 20:01:06 +0100

    initial commit

---
+++ mailverteiler/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mailverteiler::Application.config.secret_token = '8c58883e67e3a1ce7dc0429f20adc3995ee7c981595fc3862cebda07db35ef02503eb722ecb9a9bd1fc96aa98364e2c923d67e659f988b7b31e1221d5fd40e9c'
